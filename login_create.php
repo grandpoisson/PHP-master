70d1be527b3f0673bd9907fc1c0786ea4e05abdd
grandpoisson <grandpoisson@gmail.com>
2020-12-28 17:33:53 +0000

    refactored all CRUD  into functions

--- login_create.php
+++ login_create.php
@@ -3,17 +3,9 @@
 <?php 
   createRows();
 ?> 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>String functions</title>
-    <!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" 
-integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
-</head>
-<body>
+<?php include "includes/header.php";?>
+
+
 
 <div class="container">
   <div class="col-sm-6">
@@ -35,5 +27,4 @@ integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69p
   </div>
 </div>
  
-</body>
-</html>
+<?php include "includes/footer.php"?>
--- login_update.php
+++ login_update.php
@@ -1,46 +1,33 @@
 <?php include "db.php"; ?>
 <?php include "functions.php"; ?>
 
-<?php 
-    updateTable();
-?>
+<?php updateTable();?>
 
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>String functions</title>
-        <!-- Latest compiled and minified CSS -->
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" 
-        integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
-    </head>
+<?php include "includes/header.php";?>
 
-    <body>
+    <div class="container">
+        <div class="col-sm-6">
+        <h1 class="text-center">Update</h1>
+            <form action="login_update.php" method="post">
+                <div class="form-group">
+                    <label for="username">Username</label>
+                    <input type="text"name="username" class="form-control">
+                </div>
 
-        <div class="container">
-            <div class="col-sm-6">
-            <h1 class="text-center">Update</h1>
-                <form action="login_update.php" method="post">
-                    <div class="form-group">
-                        <label for="username">Username</label>
-                        <input type="text"name="username" class="form-control">
-                    </div>
+                <div class="form-group">
+                    <label for="password">Password</label>
+                    <input type="password" name="password" class="form-control">
+                </div>
 
-                    <div class="form-group">
-                        <label for="password">Password</label>
-                        <input type="password" name="password" class="form-control">
-                    </div>
-
-                    <div class="form-group">
-                        <select name="id" id="">
-                            <?php 
-                               showAllData();
-                            ?>
-                        </select>
-                    </div>
-                    <input class="btn btn-primary"  type="submit" name ="submit" value="Update"  >
-                </form>
-            </div>
+                <div class="form-group">
+                    <select name="id" id="">
+                        <?php 
+                            showAllData();
+                        ?>
+                    </select>
+                </div>
+                <input class="btn btn-primary"  type="submit" name ="submit" value="Update"  >
+            </form>
         </div>
-    </body>
-</html>
+    </div>
+    <?php include "includes/footer.php"?>
